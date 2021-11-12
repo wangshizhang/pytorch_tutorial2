c80aa8033721610ab7ef67d016e6284510bd946b
xuhaoli <xuhaoli@pbcmlg01.cm.cluster>
2021-11-12 13:44:53 +0800

    new notes

--- 02-intermediate/deep_residual_network/main_test.py
+++ 02-intermediate/deep_residual_network/main_test.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+import torch.onnx
 # from torchviz import make_dot
 
 # Device configuration
@@ -16,7 +17,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyper-parameters
 num_epochs = 80
-batch_size = 100
+batch_size = 1
 learning_rate = 0.001
 
 # Image preprocessing modules
@@ -70,8 +71,8 @@ class ResidualBlock(nn.Module):
         out = self.conv2(out)
         out = self.bn2(out)
         if self.downsample:
-            residual = self.downsample(x)#这里是使得16 channel去成为32 channel，形成residual
-        out += residual
+            residual = self.downsample(x)
+        out += residual#residual在这里体现，直连
         out = self.relu(out)
         return out
 
@@ -83,7 +84,7 @@ class ResNet(nn.Module):
         self.conv = conv3x3(3, 16)
         self.bn = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        self.layer1 = self.make_layer(block, 16, layers[0])#一个layer形成4个block
+        self.layer1 = self.make_layer(block, 16, layers[0])
         self.layer2 = self.make_layer(block, 32, layers[1], 2)
         self.layer3 = self.make_layer(block, 64, layers[2], 2)
         self.avg_pool = nn.AvgPool2d(8)
@@ -116,56 +117,58 @@ class ResNet(nn.Module):
     
 model = ResNet(ResidualBlock, [2, 2, 2]).to(device)
 
+for i, (images, labels) in enumerate(train_loader):
+    images = images.to(device)
+    labels = labels.to(device)
+    
+    print(images.shape)
+    # Forward pass
+    outputs = model(images)
+    if(i == 0):
+        torch.onnx.export(model, images,"resnet.onnx")#保存成onnx，可以展示结构
+        break
+
+# torch.save(model, 'resnet.pth')
 
 # Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# criterion = nn.CrossEntropyLoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-# For updating learning rate
-def update_lr(optimizer, lr):    
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+# # For updating learning rate
+# def update_lr(optimizer, lr):    
+#     for param_group in optimizer.param_groups:
+#         param_group['lr'] = lr
 
-# Train the model
-total_step = len(train_loader)
-curr_lr = learning_rate
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        images = images.to(device)
-        labels = labels.to(device)
+# # Train the model
+# total_step = len(train_loader)
+# curr_lr = learning_rate
+# for epoch in range(num_epochs):
+#     for i, (images, labels) in enumerate(train_loader):
+#         images = images.to(device)
+#         labels = labels.to(device)
         
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
+#         # Forward pass
+#         outputs = model(images)
+#         if(i == 0):
+#             g = make_dot(outputs)
+#             g.render('modelviz',view=False)
+#             break;
+
+#         loss = criterion(outputs, labels)
         
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+#         # Backward and optimize
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
         
-        if (i+1) % 100 == 0:
-            print ("Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}"
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+#         if (i+1) % 100 == 0:
+#             print ("Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}"
+#                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
-    # Decay learning rate
-    if (epoch+1) % 20 == 0:
-        curr_lr /= 3
-        update_lr(optimizer, curr_lr)
+#     if(epoch == 0):
+#         break;
+#     # Decay learning rate
+#     if (epoch+1) % 20 == 0:
+#         curr_lr /= 3
+#         update_lr(optimizer, curr_lr)
 
-# Test the model
-model.eval()
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-    print('Accuracy of the model on the test images: {} %'.format(100 * correct / total))
-
-# Save the model checkpoint
-torch.save(model.state_dict(), 'resnet.ckpt')
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Pytorch Tutorial Notes
+## Intermediate
+### ResNet
+- block是构建单元，make_layer是基于block的构建大单元
+- 查看model结构可以用onnx + neuron进行
